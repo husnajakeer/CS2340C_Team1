1653b963eac8d676d0b385cd7debba6dcf8d7247
Armin Moinian <44126226+AMoinian2004@users.noreply.github.com>
2023-09-22 04:17:48 -0400

    Added an exit button to start screen

--- app/src/main/java/com/example/team1game/MainActivity.java
+++ app/src/main/java/com/example/team1game/MainActivity.java
@@ -1,30 +1,26 @@
 package com.example.team1game;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    Button startButton;
+    Button startButton, quitButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // default code, sets default view
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /* start button */
-        // refered to https://www.youtube.com/watch?v=nEe7bx_IuOQ
-        startButton = (Button)findViewById(R.id.startButton);
-        startButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            // on click, switch class
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, InitialConfigScreen.class);
-                startActivity(intent);
-            }
+        startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, InitialConfigScreen.class);
+            startActivity(intent);
         });
+
+        quitButton = findViewById(R.id.quitButton);
+        quitButton.setOnClickListener(view -> finish());
     }
 }
