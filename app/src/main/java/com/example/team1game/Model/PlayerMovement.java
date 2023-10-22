5248980601a5a76462fb113743748b7aa2d7798f
husnajakeer <husnajakeer@gmail.com>
2023-10-22 02:19:50 -0400

    adding new unit tests

--- app/src/main/java/com/example/team1game/Model/PlayerMovement.java
+++ app/src/main/java/com/example/team1game/Model/PlayerMovement.java
@@ -37,6 +37,7 @@ public class PlayerMovement implements Movement, Subscriber {
         }
         return playerMovement;
     }
+    
     public void moveLeft() {
         if (canMoveLeft) {
             int x = player.getX();
