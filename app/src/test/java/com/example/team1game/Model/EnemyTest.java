84421c47688e6169518af9f4f366f1325afe8c06
Rayann Liang <rliang43@gatech.edu>
2023-11-12 00:09:36 -0500

    adding tests for Enemy

---
+++ app/src/test/java/com/example/team1game/Model/EnemyTest.java
@@ -0,0 +1,36 @@
+package com.example.team1game.Model;
+
+import junit.framework.TestCase;
+
+public class EnemyTest extends TestCase {
+
+    public void testGetDamage() {
+    }
+
+    public void testUpdate() {
+    }
+
+    public void testSetDamage() {
+    }
+
+    public void testGetMovementSpeed() {
+    }
+
+    public void testSetMovementSpeed() {
+    }
+
+    public void testGetEnemyMovement() {
+    }
+
+    public void testSetEnemyMovement() {
+    }
+
+    public void testGetGameScreen() {
+    }
+
+    public void testGetEnemyRect() {
+    }
+
+    public void testIsPlayerInContactWithEnemy() {
+    }
+}
